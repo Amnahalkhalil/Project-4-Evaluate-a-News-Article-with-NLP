c83e13755bd89c08925e49e3e8a8a9b1ca9e9347
Amnah Al.Shammari <amnahalkhalil@gmail.com>
2020-07-23 22:52:45 +0300

    make it better

---
+++ dist/precache-manifest.e016b4043a595da828a68639790d8cbc.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2cecc2ac7f0ecc1e23",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "8c2cecc2ac7f0ecc1e23",
+    "url": "main.css"
+  }
+]);
---
+++ dist/precache-manifest.e2f1b7acbea06345206ae2b6644f925f.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a65a9558e5df36dbc20e315cc74278f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c2cecc2ac7f0ecc1e23",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "8c2cecc2ac7f0ecc1e23",
+    "url": "main.css"
+  },
+  {
+    "url": "precache-manifest.e016b4043a595da828a68639790d8cbc.js"
+  },
+  {
+    "revision": "a399124f32eb6ecbb25bd06aa9c85a91",
+    "url": "service-worker.js"
+  }
+]);
